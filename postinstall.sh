5ae803425fea7e9e147875bfee9787ef22d87fdb
liweinan <l.weinan@gmail.com>
2019-07-16 11:39:32 +0800

    .

--- postinstall.sh
+++ postinstall.sh
@@ -2,4 +2,4 @@
 rm -rf /data/hibernate
 mkdir -p /data/hibernate
 ln -s /work/hibernate-orm /data/hibernate
-su - postgres -c /usr/lib/postgresql/9.6/bin/postgres -D /var/lib/postgresql/9.6/main -c config_file=/etc/postgresql/9.6/main/postgresql.conf
+su - postgres -c "/usr/lib/postgresql/9.6/bin/postgres -D /var/lib/postgresql/9.6/main -c config_file=/etc/postgresql/9.6/main/postgresql.conf"
